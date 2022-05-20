1ff1503972ea2cbe462965f81f692ee250292459
Bheemesh1706 <bheemeshbala.71@gmail.com>
2022-05-20 17:01:51 +0530

    Added Project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -9,8 +9,12 @@ import "react-carousel-animated/dist/style.css";
 function Projects() {
 
     const test = ["test1","test1","test1","test1","test1"];
-    const [projects,setProjects] =useState([{name:"Tapper — A Portfolio for MetaMask Wallet",about:"It’s a Web3 Portfolio website for your Crypto and ERC-20 tokens",git:"https://github.com/Bheemesh1706/Tapper",technology:["Web3.js","Next.js","Typescript","Chakra-UI","Rapid-API","Git"]},
-    {name:"A Custom Ecommerce",about:"It’s a FullStack MERN application with jwt authentication ,uses Context API for managing the state and hosted in netlify",git:"https://github.com/Bheemesh1706/E-Commerce-FrontEnd",technology:["Reactjs","Node","MongoDB","Context API", "Git"]},
+    const [projects,setProjects] =useState([{name:"Tapper — A Portfolio for MetaMask Wallet",about:"It’s a Web3 Portfolio website for your Crypto and ERC-20 tokens",git:"https://github.com/Bheemesh1706/Tapper",technology:["Web3.js","Next.js","Typescript","Chakra-UI","Rapid-API","Git","Netlify"]},
+    {name:"Emotion Interface",about:"An prototype for SAAS product that used to identify human emotional state during video calls in applications like Google Meet ,Zoom ,Microsoft Teams etc",git:"https://github.com/Bheemesh1706/Emotion-Interface",technology:["React.js","Flask","Node.js","Python","Javascript","GCP API","AWS","AI","Chrome Extentions"]},
+    {name:"Forest Rover",about:"An Atonomous rover for forest exploration",git:"https://github.com/Bheemesh1706/Animal_Object_Detection",technology:["Python", "Flask","AI","Embedded C"]},
+    {name:"Scrappy",about:"This Repo consist of webscrapper and bots for dedicated purposes like gmail's email parsing,wiki content scrapper etc.",git:"https://github.com/Bheemesh1706/Web-Scappers-And-Bots",technology:["Python", "Flask","Heroku"]},
+    {name:"A Custom Ecommerce",about:"It’s a FullStack MERN application with jwt authentication ,uses Context API for managing the state and hosted in netlify",git:"https://github.com/Bheemesh1706/E-Commerce-FrontEnd",technology:["Reactjs","Node","MongoDB","Context API", "Git","Netlify","Heroku"]},
+    {name:"Trio",about:"This a Chat Application that uses React as its frontend and Ruby on Rails for its Backend",git:"https://github.com/Bheemesh1706/Trio-Frontend",technology:["React.js", "Ruby on Rails","Heroku","Netlify","Git"]},
     {name:"Alpha Blog — A Blogging Website",about:"It’s a Blogging WebApp written in Ror backed by sqlLite for database and hosted in heroku.",git:"https://github.com/Bheemesh1706/Alpha-Blog",technology:["Ruby", "Sqllite","Heroku","Ruby on Rails"]}]);
 
 
